39b4c0ed7a21d62ea85e085338e10ba481d2110c
malachek <malachykennedy1@gmail.com>
2025-04-12 03:36:36 -0700

    dialogue CSV parsing

--- Assets/_Scripts/Phone/Dialogue.cs
+++ Assets/_Scripts/Phone/Dialogue.cs
@@ -1,16 +1,40 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Dialogue : MonoBehaviour
+public class DialogueController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private PlayerController _playerController;
+
     void Start()
     {
-        
+        _playerController = new();
+        Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Enable()
     {
-        
+        _playerController.PlayerActions.FirstSelection.performed += FirstAction;
+        _playerController.PlayerActions.SecondSelection.performed += SecondAction;
+        _playerController.PlayerActions.ThirdSelection.performed += ThirdAction;
+        _playerController.PlayerActions.Enable();
     }
+
+
+    void OnDisable()
+    {
+        _playerController.PlayerActions.FirstSelection.performed -= FirstAction;
+        _playerController.PlayerActions.SecondSelection.performed -= SecondAction;
+        _playerController.PlayerActions.ThirdSelection.performed -= ThirdAction;
+    }
+
+    public void FirstAction(InputAction.CallbackContext context) => TakeAction(1);
+    public void SecondAction(InputAction.CallbackContext context) => TakeAction(2);
+    public void ThirdAction(InputAction.CallbackContext context) => TakeAction(3);
+
+    private void TakeAction(int actionNum)
+    {
+        Debug.Log($"Taking Action: {actionNum}");
+    }
+
 }
