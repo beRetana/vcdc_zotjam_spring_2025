81b2c2a0e8fca196038711b490e6de840f069b47
Brandon Eduardo Retana Garcia <beretana@uci.edu>
2025-04-12 14:25:11 -0700

    phone manager

--- Assets/_Scripts/Phone/UI/MessagesManager.cs
+++ Assets/_Scripts/Phone/UI/MessagesManager.cs
@@ -1,24 +1,80 @@
 using System;
-using System.Collections;
-using JetBrains.Annotations;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MessagesManager : MonoBehaviour
 {
-    [SerializeField] private MessagesSizes playerMessagesSizes;
+    [SerializeField] private MessagesTemplates playerMessageTemplates;
+    [SerializeField] private MessagesTemplates wifeMessageTemplates;
 
     [Serializable]
-    public struct MessagesSizes
+    public struct MessagesTemplates
     {
-        
+        public Transform OneMidLine;
+        public Transform OneLine;
+        public Transform TwoLine;
+        public Transform ThreeLine;
     }
 
-    private Queue messagesHistory;
+    private Queue<Transform> messagesHistory;
+    private const int MAX_MESSAGES = 12;
+    private int messagesCount;
 
     void Start ()
     {
-        messagesHistory = new Queue();
+        messagesHistory = new Queue<Transform>();
     }
 
+    public void SendMessage(CSVReader.DialogueRow dialogueRow)
+    {
+        switch (dialogueRow.character)
+        {
+            case CSVReader.CharacterEnum.Her:
+                ComposeMessage(wifeMessageTemplates, dialogueRow.dialogue);
+                break;
+            case CSVReader.CharacterEnum.Me:
+                ComposeMessage(playerMessageTemplates, dialogueRow.dialogue);
+                break;
+            case CSVReader.CharacterEnum.None:
+                break;
+        }
+    }
+
+    private void ComposeMessage(MessagesTemplates template, string content)
+    {
+        if(content.Length <= 11)
+        {
+            DisplayMessage(template.OneMidLine, content);
+        }
+        else if (content.Length <= 23)
+        {
+            DisplayMessage(template.OneMidLine, content);
+        }
+        else if (content.Length <= 46)
+        {
+            DisplayMessage(template.OneMidLine, content);
+        }
+        else if (content.Length <= 69)
+        {
+            DisplayMessage(template.OneMidLine, content);
+        }
+        else
+        {
+            DisplayMessage(template.OneMidLine, content.Substring(0, 69));
+            ComposeMessage(template,content.Substring(69));
+        }
+    }
+
+    private void DisplayMessage(Transform messageTemplateType, string content)
+    {
+        if (messagesCount >= MAX_MESSAGES)
+            messagesHistory.Dequeue();
+        else ++messagesCount;
+        
+        Transform newMessage = Instantiate(messageTemplateType, transform);
+        newMessage.GetChild(0).GetComponent<TextMeshProUGUI>().text = content;
+        messagesHistory.Enqueue(newMessage);
+    }
 
 }
