319229c58b291538539372bc2704988da4ed5d05
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-04-13 01:40:10 -0700

    Enemies base done

--- Assets/_Scripts/Enemies/AI_FSM/Tasks/EnemyAttack.cs
+++ Assets/_Scripts/Enemies/AI_FSM/Tasks/EnemyAttack.cs
@@ -3,7 +3,7 @@ using AI_FSM;
 
 public abstract class EnemyAttack : TaskBase
 {
-    [SerializeField, Range(0f,100f)] protected float _damage;
+    [SerializeField, Range(0f,100f)] protected int _damage;
     [SerializeField, Range(0f, 100f)] protected float _range;
     [SerializeField, Range(0f, 100f)] protected float _attackArea;
 
--- Assets/_Scripts/Enemies/AI_FSM/Tasks/QuickMeleeAttack.cs
+++ Assets/_Scripts/Enemies/AI_FSM/Tasks/QuickMeleeAttack.cs
@@ -1,18 +1,54 @@
 using UnityEngine;
+using System.Collections;
+using static UnityEngine.UI.Image;
 
 public class QuickMeleeAttack : EnemyAttack
 {
+    [SerializeField] protected float _timeBetweenAttacks = .1f;
+
+    protected Enemy _enemy;
+
+    protected override void Start()
+    {
+        _enemy = GetComponent<Enemy>();
+    }
+
     public override void Enable()
     {
-        base.Enable();
+        Attack();
     }
+
+    public override void Disable()
+    {
+        StopCoroutine(AttackTimer());
+    }
+
     public override void Attack()
     {
-        float direction = GetPlayerDirection().x;
-        direction /= Mathf.Abs(direction);
         RaycastHit hit;
-        if (!Physics.SphereCast(transform.position, _attackArea, Vector3.right * direction, out hit, _range)) return;
-        
+        Vector3 direction = GetPlayerDirection();
+        if (Physics.Raycast(transform.position  + Vector3.up, direction, out hit, _range))
+        {
+            Debug.Log($"Attaking: {hit.transform.gameObject.name}");
+            Damage(hit.transform.GetComponent<Health>(), _damage);
+        }
+        StartCoroutine(AttackTimer());
+    }
+
+    private void Damage(Health health, int damage)
+    {
+        while (damage > 0)
+        {
+            health?.LoseHealth();
+            --damage;
+        }
+    }
+
+    IEnumerator AttackTimer()
+    {
+        yield return new WaitForSecondsRealtime(_timeBetweenAttacks);
+        if (_enemy.CloseEnough()) Attack();
+        else _enemy.UpdateBehaviour(Enemy.EnemyState.Chasing);
     }
 
     private Vector3 GetPlayerDirection()
