f1f014c5ca1d8470ac7f9b37a00c307a86d0446c
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-04-13 15:34:45 -0700

    new name

--- Assets/_Scripts/Menus/MainMenu.cs
+++ Assets/_Scripts/Menus/MainMenu.cs
@@ -6,7 +6,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Stage 1");
+        SceneManager.LoadScene("Forest_F1");
     }
 
     public void QuitGame()
